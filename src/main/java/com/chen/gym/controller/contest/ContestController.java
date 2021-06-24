854c9aec65cd1bc68b4187814c4b04596660ccbc
XS_Jinbert <mr_jinbert@163.com>
2021-06-24 14:40:08 +0800

    部分文档微调

--- src/main/java/com/chen/gym/controller/contest/ContestController.java
+++ src/main/java/com/chen/gym/controller/contest/ContestController.java
@@ -113,12 +113,12 @@ public class ContestController {
             @ApiImplicitParam(name = "name", value = "赛事名字", required = true, paramType = "query", dataType = "String"),
             @ApiImplicitParam(name = "organizers", value = "赛事主办方", required = true, paramType = "query", dataType = "String"),
             @ApiImplicitParam(name = "openTime", value = "赛事开始时间", required = true, paramType = "query", dataType = "Date"),
-            @ApiImplicitParam(name = "date", value = "赛事日期", required = true, paramType = "query", dataType = "Date"),
+            @ApiImplicitParam(name = "date", value = "编辑时间", required = false, paramType = "query", dataType = "Date"),
             @ApiImplicitParam(name = "object", value = "参赛对象", required = true, paramType = "query", dataType = "String"),
             @ApiImplicitParam(name = "tele", value = "赛事主办方电话", required = true, paramType = "query", dataType = "String"),
             @ApiImplicitParam(name = "place", value = "赛事场地使用ID", required = false, paramType = "query", dataType = "String"),
             @ApiImplicitParam(name = "equipment", value = "赛事器材使用id", required = false, paramType = "query", dataType = "String"),
-            @ApiImplicitParam(name = "target", value = "标签状态：1、刚创建未复核；2、已复核", required = true, paramType = "query", dataType = "number"),
+            @ApiImplicitParam(name = "target", value = "标签状态：1、刚创建未复核；2、已复核", required = false, paramType = "query", dataType = "number"),
     })
     public JsonResult add(Contest contest) {
         contestService.add(contest);
